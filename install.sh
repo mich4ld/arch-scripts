a6503bf11d01ffcc97cf536356e15c0824664438
Michał <43048524+mich4ld@users.noreply.github.com>
2021-08-14 13:51:27 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -44,6 +44,7 @@ function configure_dev() {
    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
    git clone https://github.com/agkozak/zsh-z $ZSH_CUSTOM/plugins/zsh-z
    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+   cp ./configs/.zshrc ~/.zshrc
    
    echo "Your email for git:"
    read git_email
