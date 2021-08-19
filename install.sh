1ad8300165240e9b2360c3c901273bc304383680
Michał <43048524+mich4ld@users.noreply.github.com>
2021-08-19 23:29:15 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,15 +2,15 @@ FONTS_DIR=~/.local/share/fonts/
 cd "$(dirname "$0")"
 
 function install_base() {
-    echo "Installing basic modules..."
+    echo "===> Installing basic modules..."
     sudo pacman -S xdg-user-dirs git nano vim fuse unzip gvfs gvfs-mtp gvfs-afc gvfs-nfs gvfs-smb openssh jre11-openjdk python-pip base-devel cmake alsa-plugins alsa-utils pamixer pulseaudio
     
-    echo "Generating local home folders..."
+    echo "===> Generating local home folders..."
     xdg-user-dirs-update
 }
 
 function install_fonts() {
-   echo "Installing fonts..."
+   echo "===> Installing fonts..."
    sudo pacman -S noto-fonts-emoji ttf-roboto-mono ttf-liberation ttf-droid
    mkdir -p $FONTS_DIR
    cp -v ./fonts/* $FONTS_DIR
@@ -18,7 +18,7 @@ function install_fonts() {
 }
 
 function install_aur() {
-   echo "Install AUR helper..."
+   echo "===> Install AUR helper..."
    git clone https://aur.archlinux.org/paru-bin.git
    cd paru-bin
    makepkg -si
@@ -26,34 +26,34 @@ function install_aur() {
 }
 
 function install_tools() {
-   echo "Install tools and utils..."
+   echo "===> Install tools and utils..."
    sudo pacman -S neofetch cmatrix htop gparted redshift flameshot youtube-dl
 }
 
 function configure_dev() {
-   echo "Installing development tools..."
+   echo "===> Installing development tools..."
    sudo pacman -S docker docker-compose jq whois zsh
    
-   echo "Configuring Docker..."
+   echo "===> Configuring Docker..."
    sudo systemctl start docker.service
    sudo systemctl enable docker.service
    sudo usermod -aG docker $USER
    
-   echo "Your email for git:"
+   echo "===> Your email for git:"
    read git_email
 
-   echo "Your name for git:"
+   echo "===> Your name for git:"
    read git_name
 
    git config --global user.email $git_email
    git config --global user.name $git_name
 
-   echo "Installing development tools from AUR..."
+   echo "===> Installing development tools from AUR..."
    paru -S mongodb-compass visual-studio-code-bin insomnia-bin
 }
 
 function install_browsers() {
-   echo "Installing browsers..."
+   echo "===> Installing browsers..."
    paru -S firefox brave-bin
 }
 
